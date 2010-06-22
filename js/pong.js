583e1381f8902ef88c54122b84d05f972d8a2a33
Yann Thomas-Gerard <inside@gmail.com>
2010-06-22 17:04:21 +0200

    Overlap is ok and refactor the code

--- js/pong.js
+++ js/pong.js
@@ -2,13 +2,13 @@
 function hoverlap(rect1, rect2)
 {
 //    boolean hoverlap = (x1<x2+w2) && (x2<x1+w1);
-    return (rect1.el.offsetLeft < rect2.el.offsetLeft + rect2.width) && (rect2.el.offsetLeft < rect1.el.offsetLeft + rect1.width);
+    return (rect1.x < rect2.x + rect2.width) && (rect2.x < rect1.x + rect1.width);
 }
 
 function voverlap(rect1, rect2)
 {
 //    boolean voverlap = (y1<y2+h2) && (y2<y1+h1);
-    return (rect1.el.offsetTop < rect2.el.offsetTop + rect2.height) && (rect2.el.offsetTop <rect1.el.offsetTop + rect1.height)
+    return (rect1.y < rect2.y + rect2.height) && (rect2.y < rect1.y + rect1.height)
 }
 
 function overlap(rect1, rect2)
@@ -19,6 +19,8 @@ function overlap(rect1, rect2)
 Pong = function()
 {
     this.gameArea      = null;
+    this.intervalId    = null;
+    this.isPaused      = false;
     this.width         = 200;
     this.height        = 200;
     this.lp            = null;
@@ -39,7 +41,9 @@ Pong = function()
             'container': this,
             'id': 'pong-left',
             'width': 10,
-            'height': 50
+            'height': 50,
+            'x': 0,
+            'y': 70,
         });
         this.lp.init();
         this.rp = new Paddle(
@@ -47,14 +51,17 @@ Pong = function()
             'container': this,
             'id': 'pong-right',
             'width': 10,
-            'height': 50
+            'height': 50,
+            'x': 190,
+            'y': 70,
         });
         this.rp.init();
         this.ball = new Ball(
         {
             'container': this,
             'id': 'pong-ball',
-            'size': 10,
+            'width': 10,
+            'height': 10,
             'x': 90,
             'y': 90,
         });
@@ -64,7 +71,7 @@ Pong = function()
         document.observe('keydown', this.keydownHandler.bind(this));
         document.observe('keyup', this.keyupHandler.bind(this));
 
-        setInterval(this.update.bind(this), 30);
+        this.start();
     };
 
     this.update = function()
@@ -101,7 +108,7 @@ Pong = function()
             this.isLpGoingUp = false;
             this.isLpGoingDown = true;
         }
-        if (e.keyCode == 38) // Up arrow
+        else if (e.keyCode == 38) // Up arrow
         {
             this.isRpGoingUp = true;
             this.isRpGoingDown = false;
@@ -119,17 +126,46 @@ Pong = function()
         {
             this.isLpGoingUp = false;
         }
-        if (e.keyCode == 81) // Q
+        else if (e.keyCode == 81) // Q
         {
             this.isLpGoingDown = false;
         }
-        if (e.keyCode == 38) // Up arrow
+        else if (e.keyCode == 38) // Up arrow
         {
             this.isRpGoingUp = false;
         }
-        if (e.keyCode == 40) // Down arrow
+        else if (e.keyCode == 40) // Down arrow
         {
             this.isRpGoingDown = false;
+        }
+        else if (e.keyCode == 80) // P pause
+        {
+            this.toggleStartPause();
+        }
+    };
+
+    this.start = function()
+    {
+        this.intervalId = setInterval(this.update.bind(this), 30);
+    };
+
+    this.stop = function()
+    {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    };
+
+    this.toggleStartPause = function()
+    {
+        if (this.isPaused)
+        {
+            this.isPaused = false;
+            this.start();
+        }
+        else
+        {
+            this.isPaused = true;
+            this.stop();
         }
     };
 };
@@ -139,6 +175,8 @@ Paddle = function(p)
     this.id        = p.id;
     this.width     = p.width;
     this.height    = p.height;
+    this.x         = p.x;
+    this.y         = p.y;
     this.container = p.container;
     this.speed     = 10;
     this.el        = null;
@@ -150,110 +188,32 @@ Paddle = function(p)
         el.setAttribute('style', 'width: ' + this.width + 'px; height: ' + this.height + 'px;');
         this.container.gameArea.appendChild(el);
         this.el = $(this.id);
+        this.setPosition(this.x, this.y);
     };
 
     this.moveUp = function()
     {
-        if (this.el.offsetTop <= 0)
+        var y = this.y - this.speed;
+
+        if (y <= 0)
         {
-            return;
+            this.y = 0
+        }
+        else
+        {
+            this.y = y;
         }
 
-        this.el.style.top = (this.el.offsetTop - this.speed) + 'px';
+        this.setPosition(this.x, this.y);
     };
 
     this.moveDown = function()
     {
-        if (this.el.offsetTop >= this.container.height - this.height)
-        {
-            return;
-        }
+        var y = this.y + this.speed;
 
-        this.el.style.top = (this.el.offsetTop + this.speed) + 'px';
-    };
-};
-
-Ball = function(p)
-{
-    this.id        = p.id;
-    this.container = p.container;
-    this.size      = this.width = this.height = p.size;
-    this.halfSize  = p.size / 2;
-    this.x         = p.x;
-    this.y         = p.y;
-    this.speed     = 3;
-    this.vX        = 1;
-    this.vY        = 0.5;
-    this.el        = null;
-
-    this.init = function()
-    {
-        var el = document.createElement('div');
-        el.setAttribute('id', this.id);
-        el.setAttribute('style', 'width: ' + this.size + 'px; height: ' + this.size + 'px;');
-        this.container.gameArea.appendChild(el);
-        this.el = $(this.id);
-        this.setPosition(this.x, this.y);
-    };
-
-    this.move = function()
-    {
-//        def overlaps?(rect)
-//            (rect.right >= @left and rect.left <= @right and rect.bottom >= @top and rect.top <= @bottom)
-//              end
-        var x = this.x + (this.vX * this.speed);
-        var y = this.y + (this.vY * this.speed);
-
-        // Left and right walls
-        if (x <= this.halfSize)
+        if (y >= this.container.height - this.height)
         {
-            this.x = this.halfSize;
-            console.log('left player you loose');
-        }
-        else if (x >= this.container.width - this.halfSize)
-        {
-            this.x = this.container.width - this.halfSize;
-            console.log('right player you loose');
-        }
-        else
-        {
-            this.x = x;
-        }
-
-        // Paddles rebound
-        if (overlap(this.container.lp, this))
-        {
-            this.vX *= -1;
-        }
-//        if (this.x <= (this.halfSize) + this.container.lp.width)
-//        {
-//            if (this.y >= $(this.container.lp.id).offsetTop && this.y <= $(this.container.lp.id).offsetTop + this.container.lp.height)
-//            {
-//                this.vX *= -1;
-//            }
-//        }
-        if (overlap(this.container.rp, this))
-        {
-            this.vX *= -1;
-        }
-//        if (this.x >= (this.container.width - this.halfSize) - this.container.rp.width)
-//        {
-//            if (this.y >= $(this.container.rp.id).offsetTop && this.y <= $(this.container.rp.id).offsetTop + this.container.rp.height)
-//            {
-//                this.vX *= -1;
-//            }
-//        }
-
-        // Floor and ceiling
-        if (y <= this.halfSize)
-        {
-            this.vY *= -1;
-            this.y = this.halfSize;
-        }
-        else if (y >= this.container.height - this.halfSize)
-        {
-            this.vY *= -1;
-            this.y = this.container.height - this.halfSize;
+            this.y = this.container.height - this.height;
         }
         else
         {
@@ -265,8 +225,90 @@ Ball = function(p)
 
     this.setPosition = function(x, y)
     {
-       this.el.style.left = (x - this.halfSize) + 'px';
-       this.el.style.top = (y - this.halfSize)  + 'px';
+       this.el.style.left = x  + 'px';
+       this.el.style.top  = y  + 'px';
+    };
+};
+
+Ball = function(p)
+{
+    this.id        = p.id;
+    this.container = p.container;
+    this.width     = p.width;
+    this.height    = p.height;
+    this.x         = p.x;
+    this.y         = p.y;
+    this.speed     = 3;
+    this.vX        = 1;
+    this.vY        = 0.5;
+    this.el        = null;
+
+    this.init = function()
+    {
+        var el = document.createElement('div');
+        el.setAttribute('id', this.id);
+        el.setAttribute('style', 'width: ' + this.width + 'px; height: ' + this.height + 'px;');
+        this.container.gameArea.appendChild(el);
+        this.el = $(this.id);
+        this.setPosition(this.x, this.y);
+    };
+
+    this.move = function()
+    {
+        var x = this.x + (this.vX * this.speed);
+        var y = this.y + (this.vY * this.speed);
+
+        // Left and right walls
+        if (x <= 0)
+        {
+            this.x = 0;
+            console.log('left player you loose');
+        }
+        else if (x >= this.container.width - this.width)
+        {
+            this.x = this.container.width - this.width;
+            console.log('right player you loose');
+        }
+        else
+        {
+            this.x = x;
+        }
+
+        // Paddles rebound
+        if (overlap(this.container.lp, this))
+        {
+            this.x = this.container.lp.x + this.container.lp.width;
+            this.vX *= -1;
+        }
+        else if (overlap(this.container.rp, this))
+        {
+            this.x = (this.container.rp.x - this.width) + (this.vX * this.speed);
+            this.vX *= -1;
+        }
+
+        // Floor and ceiling
+        if (y <= 0)
+        {
+            this.vY *= -1;
+            this.y = 0;
+        }
+        else if (y >= this.container.height - this.height)
+        {
+            this.vY *= -1;
+            this.y = this.container.height - this.height;
+        }
+        else
+        {
+            this.y = y;
+        }
+
+        this.setPosition(this.x, this.y);
+    };
+
+    this.setPosition = function(x, y)
+    {
+       this.el.style.left = x + 'px';
+       this.el.style.top  = y + 'px';
     };
 }
 
