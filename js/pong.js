9fb770a0f935337d78958a8e8b17b4e85f432318
Yann Thomas-Gerard <inside@gmail.com>
2010-06-29 16:09:00 +0200

    Handles pausing starting stopping

--- js/pong.js
+++ js/pong.js
@@ -2,12 +2,14 @@ var Pong = Class.create(
 {
     gameArea      : null,
     intervalId    : null,
-    isPaused      : false,
     width         : 200,
     height        : 200,
     lp            : null,
     rp            : null,
     ball          : null,
+    paused        : false,
+    started       : false,
+    stopped       : true,
 
     initialize: function(p)
     {
@@ -17,37 +19,31 @@ var Pong = Class.create(
         document.getElementsByTagName('body')[0].appendChild(this.gameArea);
         this.lp = new Paddle(
         {
-            'container': this,
-            'id': 'pong-left',
-            'width': 10,
-            'height': 50,
-            'x': 0,
-            'y': 70,
+            'container' : this,
+            'id'        : 'pong-left',
+            'width'     : 10,
+            'height'    : 50,
+            'position'  : 'left'
         });
         this.rp = new Paddle(
         {
-            'container': this,
-            'id': 'pong-right',
-            'width': 10,
-            'height': 50,
-            'x': 190,
-            'y': 70,
+            'container' : this,
+            'id'        : 'pong-right',
+            'width'     : 10,
+            'height'    : 50,
+            'position'  : 'right'
         });
         this.ball = new Ball(
         {
-            'container': this,
-            'id': 'pong-ball',
-            'width': 10,
-            'height': 10,
-            'x': 90,
-            'y': 90,
+            'container' : this,
+            'id'        : 'pong-ball',
+            'width'     : 10,
+            'height'    : 10,
         });
 
         // handle keystrokes
         document.observe('keydown', this.keydownHandler.bind(this));
         document.observe('keyup', this.keyupHandler.bind(this));
-
-        this.start();
     },
     update: function()
     {
@@ -113,29 +109,65 @@ var Pong = Class.create(
         }
         else if (e.keyCode == 80) // P pause
         {
-            this.toggleStartStop();
+            this.togglePause();
+        }
+        else if (e.keyCode == 83) // S stop
+        {
+            this.stop();
+        }
+        else if (e.keyCode == 32) // Space bar start
+        {
+            this.start();
         }
     },
     start: function()
     {
-        this.intervalId = setInterval(this.update.bind(this), 30);
+        if (this.started)
+        {
+            return;
+        }
+        this.stop();
+        this.pursue();
     },
     stop: function()
     {
+        this.started    = false;
+        this.stopped    = true;
+        this.paused     = true;
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+        this.ball.resetPosition();
+        this.lp.resetPosition();
+        this.rp.resetPosition();
+    },
+    pause: function()
+    {
+        this.started    = true;
+        this.stopped    = false;
+        this.paused     = true;
         clearInterval(this.intervalId);
         this.intervalId = null;
     },
-    toggleStartStop: function()
+    pursue: function()
     {
-        if (this.isPaused)
+        this.started    = true;
+        this.stopped    = false;
+        this.paused     = false;
+        this.intervalId = setInterval(this.update.bind(this), 30);
+    },
+    togglePause: function()
+    {
+        if (this.stopped)
         {
-            this.isPaused = false;
-            this.start();
+            return;
+        }
+        if (this.paused)
+        {
+            this.pursue();
         }
         else
         {
-            this.isPaused = true;
-            this.stop();
+            this.pause();
         }
     }
 });
@@ -191,10 +223,28 @@ var Paddle = Class.create(Equipement,
     speed       : 10,
     isGoingUp   : false,
     isGoingDown : false,
+    position    : null,
 
     initialize: function($super, p)
     {
+        this.position = p.position;
         $super(p);
+        this.resetPosition();
+    },
+    resetPosition: function()
+    {
+        this.y = this.container.height / 2 - this.height / 2;
+
+        if (this.position === 'right')
+        {
+            this.x = this.container.width - this.width;
+        }
+        else if (this.position === 'left')
+        {
+            this.x = 0;
+        }
+
+        this.setPosition(this.x, this.y);
     },
     moveUp: function()
     {
@@ -241,44 +291,53 @@ var Ball = Class.create(Equipement,
 
     initialize: function($super, p)
     {
+        $super(p);
+        this.resetPosition();
+    },
+    resetPosition: function()
+    {
+        this.x = (this.container.width / 2) - (this.width / 2);
+        this.y = (this.container.height / 2) - (this.height / 2);
+        this.V = $V([1, 0]);
         this.vX = this.V.e(1);
         this.vY = this.V.e(2);
-        $super(p);
+        this.setPosition(this.x, this.y);
     },
     move: function()
     {
         var x = this.x + (this.vX * this.speed);
         var y = this.y + (this.vY * this.speed);
 
-        // Left and right walls
+        this.handleLeftAndRightWallsRebound(x);
+        this.handlePaddleRebound();
+        this.handleFloorAndCeilingRebound(y);
+        
+        this.V = $V([this.vX, this.vY]).toUnitVector();
+        this.vX = this.V.e(1);
+        this.vY = this.V.e(2);
+        this.setPosition(this.x, this.y);
+    },
+    handleLeftAndRightWallsRebound: function(x)
+    {
         if (x <= 0)
         {
             this.x = 0;
+            this.container.pause();
             console.log('Left player you loose.');
         }
         else if (x >= this.container.width - this.width)
         {
             this.x = this.container.width - this.width;
+            this.container.pause();
             console.log('Right player you loose.');
         }
         else
         {
             this.x = x;
         }
-
-        // Paddles rebound
-        if (this.overlap(this.container.lp))
-        {
-            this.x = this.container.lp.x + this.container.lp.width;
-            this.reboundsOnPaddle(this.container.lp);
-        }
-        else if (this.overlap(this.container.rp))
-        {
-            this.x = this.container.rp.x - this.width;
-            this.reboundsOnPaddle(this.container.rp);
-        }
-
-        // Floor and ceiling
+    },
+    handleFloorAndCeilingRebound: function(y)
+    {
         if (y <= 0)
         {
             this.vY *= -1;
@@ -293,11 +352,19 @@ var Ball = Class.create(Equipement,
         {
             this.y = y;
         }
-
-        this.V = $V([this.vX, this.vY]).toUnitVector();
-        this.vX = this.V.e(1);
-        this.vY = this.V.e(2);
-        this.setPosition(this.x, this.y);
+    },
+    handlePaddleRebound: function()
+    {
+        if (this.overlap(this.container.lp))
+        {
+            this.x = this.container.lp.x + this.container.lp.width;
+            this.reboundsOnPaddle(this.container.lp);
+        }
+        else if (this.overlap(this.container.rp))
+        {
+            this.x = this.container.rp.x - this.width;
+            this.reboundsOnPaddle(this.container.rp);
+        }
     },
     reboundsOnPaddle: function(paddle)
     {
