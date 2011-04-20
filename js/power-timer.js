4a3434a8305ae65de8dbe69411e1f5fbd0f2b16f
Yann Thomas-Gerard <inside@gmail.com>
2011-04-20 10:53:46 +0200

    Puts handlePowerTimer where it should be

--- js/power-timer.js
+++ js/power-timer.js
@@ -1,5 +1,30 @@
 var PowerTimer =
 {
     leftPaddlePowers: $H(),
-    rightPaddlePowers: $H()
+    rightPaddlePowers: $H(),
+    handlePowerTimer: function(context)
+    {
+        if (PowerTimer.leftPaddlePowers.size() > 0)
+        {
+            PowerTimer.leftPaddlePowers.each(function(pair)
+            {
+                if (pair.value[0] + POWER_LIFETIME <= context.startTime)
+                {
+                    pair.value[1]();
+                    PowerTimer.leftPaddlePowers.unset(pair.key);
+                }
+            }, context);
+        }
+        if (PowerTimer.rightPaddlePowers.size() > 0)
+        {
+            PowerTimer.rightPaddlePowers.each(function(pair)
+            {
+                if (pair.value[0] + POWER_LIFETIME <= context.startTime)
+                {
+                    pair.value[1]();
+                    PowerTimer.rightPaddlePowers.unset(pair.key);
+                }
+            }, context);
+        }
+    }
 };
